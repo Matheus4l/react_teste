9c3fe45293656467132da5a6aef5895be980ba7b
mac3c <mac3c@MacBook-Air-de-mac3c.local>
2019-08-08 18:23:56 -0300

    mais informacoes da api

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -3,17 +3,18 @@ import Api from '../../services/App'
 import { async } from 'q';
 import apiStarWars from '../../services/App';
 import './style.css';
-
+import Button from 'react-bootstrap/Button';
 
 export default class Main extends Component{
 
     state={
-      planets:[]
+      planets:[],
+      num  : 1
     };
 
     componentDidMount(){
-
-        this.loadPlanets('3');
+        
+        this.loadPlanets(this.state.num);
     };
 
     
@@ -29,7 +30,7 @@ export default class Main extends Component{
                      <tr>
                        <th scope="row">1</th>
                        <td>População</td>
-                       <td>{planets.population}</td>
+                       <td>{planets.population} Pessoas</td>
                      
                      </tr>
                      <tr>
@@ -41,18 +42,45 @@ export default class Main extends Component{
                      <tr>
                        <th scope="row">3</th>
                        <td>Diamentro</td>
-                       <td>{planets.diameter}</td>
+                       <td>{planets.diameter} Km</td>
                       
                      </tr>
+                     <tr>
+                     <th scope="row">4</th>
+                     <td> Período de rotação</td>
+                     <td>{planets.rotation_period}Horas</td>
+                    
+                   </tr>
+                   <tr>
+                     <th scope="row">4</th>
+                     <td> Período orbital </td>
+                     <td>{planets.orbital_period} Dias</td>
+                    
+                   </tr>
+                   <tr>
+                   <th scope="row">4</th>
+                   <td> Agua da superfície </td>
+                   <td>{planets.surface_water}%</td>
+                  
+                 </tr>
+                    
                    </tbody>
                  </table>
+                    <Button  onClick={this.proximo} variant="outline-primary">Proximo</Button>
                 </article>
+                
         </div>
+        
+
+        
     }
 
-
+    proximo = ()=>{
+      this.state.num +=1;
+      this.loadPlanets( this.state.num);
+    }
     loadPlanets= async(num)=>{
-        console .log('o');
+
         const response =await apiStarWars.get('planets/'+num+'/');
         console .log(response.data);
          this.setState({planets: response.data})
